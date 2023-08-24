f892cf584720d1c6b0af5118a0ab2e4056011872
Crowdbotics <engineering@crowdbotics.com>
2023-08-24 10:53:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-bonus-dev-111173",
+  url: "https://green-bonus-dev-111173.botics.co",
+  api: "https://green-bonus-dev-111173.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
